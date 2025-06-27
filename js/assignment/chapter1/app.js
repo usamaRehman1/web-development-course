60a5b1d93ee4f01b784dd045b1e5e81e1f5e2cfd
Muhammad Usama <46845901+usamaRehman1@users.noreply.github.com>
2025-06-27 01:08:28 -0700

    chapter 1 assignment

---
+++ js/assignment/chapter1/app.js
@@ -0,0 +1,6 @@
+ alert("Welcome!");
+ alert("Error! Please enter valid password.");
+ alert("Welcome to JS land \n Happy coding.");
+ alert("Welcome to JS land");
+ alert("Happy coding.\n Prevent this page from creating aditional dialogs.");
+ alert("Hello i can run JS through my web browser`s console.");
