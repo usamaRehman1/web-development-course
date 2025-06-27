4e4f76ee8a14f778f6d06a78de6f64b29e463f14
Muhammad Usama <46845901+usamaRehman1@users.noreply.github.com>
2025-06-27 01:25:08 -0700

    chapter 2 assignment

---
+++ js/assignment/chapter2/app.js
@@ -0,0 +1,25 @@
+//VARIABLE FOR STRING
+var userName;
+var myName = "Muhammad Usama";
+var message;
+message = "Hello World";
+alert(message)
+
+//Student data 
+var stuName =  "Jhone Doe";
+var stuAge = "15 Years Old";
+var stuCertificate = "certified Mobile Web Application Development";
+
+var task5 = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+alert(task5)
+
+var email = "example@gmail.com";
+alert("My emal Adress is " + email);
+
+var book = "A Smater Way To Learn JavaScript";
+alert("I'am trying to learn from the book " + book);
+
+document.writeln("Yeh! I can write HTML content through javaScript");
+
+var design = "==========&$^^$&==========";
+alert(design);
