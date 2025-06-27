e685dfb3831bc41078014e9ebd121910d9a5083f
Muhammad Usama <46845901+usamaRehman1@users.noreply.github.com>
2025-06-27 10:27:32 -0700

    chapter 4 assignment

---
+++ js/assignment/chapter4/app.js
@@ -0,0 +1,16 @@
+//  VARIABLE NAMES: LEGAL & ILLEGAL
+
+var name, age, contact;
+
+// ----------LEGAL NAMES-------------
+var variable, _variable, $variable, variable123 , varAble ;
+
+// ----------ILLEGAL NAMES-------------
+// var 123variable, user name, user-name, @variable, variable-;
+
+document.writeln(`<h1> Rules For Naming Js Variable</h1>
+    <br>
+    Variable name can only contain number , $ and _. For Example $my_1stVariable<br>
+    Variable must begin with a letter, $ and _. For Example name , $name, _name<br>
+    Variable name are case sensitive.<br>
+    Variable name should not be JS keyword.`)
