43309c35a7c2014f21b62af0a9db75a3b0c0eaaa
Muhammad Usama <46845901+usamaRehman1@users.noreply.github.com>
2025-06-27 09:52:35 -0700

    chapter 3 assignment

---
+++ js/assignment/chapter3/app.js
@@ -0,0 +1,16 @@
+// VARIABLE FOR NUMBER
+
+var age = 15;
+alert(`I am ${age} years old.`);
+
+var num = 1;
+alert(`You have visited this site ${num} times`);
+
+var birthYear;
+birthYear = 1997;
+document.writeln(`My Birth Year is ${birthYear} <br> Data type of my declared variable is ${typeof birthYear}`)
+
+var visitorName = "John Doe";
+var productTitle = "T-shirt";
+var quantity = 5;
+document.writeln(`<br>${visitorName} ordered ${quantity} ${productTitle} on XYZ clothing store.`);
