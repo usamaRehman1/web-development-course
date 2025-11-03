589d665894ceb4b3bfab0dbc6847a03723ceea1c
Muhammad Usama <46845901+usamaRehman1@users.noreply.github.com>
2025-11-03 12:00:41 -0800

    update grocery Bud

--- groceryBud/app.js
+++ groceryBud/app.js
@@ -4,12 +4,42 @@ const showPopUp = (_icon, _title) => {
     Swal.fire({
         icon: _icon,
         title: _title,
-        text: "Something went wrong!",
+        // text: "Something went wrong!",
     });
 }
 
+const groceryList = document.querySelector("#grocery-list");
 const handleChange = () => {
-    let grocery = document.querySelector("#grocery");
+    const grocery = document.querySelector("#grocery");
 
-    if (grocery.value.trim() == "") showPopUp("error", "Oops...")
+    if (grocery.value.trim() == "") showPopUp("error", "Invalid input")
+
+    const li = document.createElement("li");
+    li.innerHTML = `<span class="listText">${grocery.value.trim()}</span>
+                        <span class="actions">
+                            <i class="fa-solid fa-pen-to-square edit" onclick="editBtn(this)"></i>
+                            <i class="fa-solid fa-trash delete" onclick="deleteBtn(this)"></i>
+                    </span>`
+    groceryList.prepend(li)
+
+    document.querySelector("#clear-btn").style.display = "block"
+    grocery.value = ""
+}
+
+const editBtn = (e) => {
+    console.log(e.parentElement.previousElementSibling)
+
+}
+
+const deleteBtn = (e) => {
+    e.parentElement.parentElement.remove()
+}
+
+const crearAll = () => {
+    [...groceryList.children].forEach( element => {
+        console.log(element)
+        element.remove()
+    });
+
+    document.querySelector("#clear-btn").style.display = "none"
 }
